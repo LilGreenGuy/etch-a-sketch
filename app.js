8a842cd0dff49bf1dfa7c0bfb0ee370af6a3b869
Nick H <nickbhenderson95@gmail.com>
2025-10-07 08:54:35 -0500

    Added touchscreen support to the Etch-A-Sketch and fixed some styling on mobile.

--- app.js
+++ app.js
@@ -1,249 +1,257 @@
-
 const inputs = {
-  clearBtn: document.querySelector('#clear'),
-  colorPicker: document.querySelector('#colorPicker'),
-  eraserBtn: document.querySelector('#erase'),
-  fillBtn: document.querySelector('#fill'),
-  incShadeBtn: document.querySelector('#incShade'),
-  decShadeBtn: document.querySelector('#decShade'),
-  rainbowBtn: document.querySelector('#rainbow'),
-  sizeSlider: document.querySelector('#slider'),
-  hexBox: document.querySelector('#hex'),
-  rgbBox: document.querySelector('#rgb'),
-  hslBox: document.querySelector('#hsl')
+	clearBtn: document.querySelector("#clear"),
+	colorPicker: document.querySelector("#colorPicker"),
+	eraserBtn: document.querySelector("#erase"),
+	fillBtn: document.querySelector("#fill"),
+	incShadeBtn: document.querySelector("#incShade"),
+	decShadeBtn: document.querySelector("#decShade"),
+	rainbowBtn: document.querySelector("#rainbow"),
+	sizeSlider: document.querySelector("#slider"),
+	hexBox: document.querySelector("#hex"),
+	rgbBox: document.querySelector("#rgb"),
+	hslBox: document.querySelector("#hsl"),
 };
 
 for (let input in inputs) {
-  if (inputs[input] === inputs.eraserBtn
-    || inputs[input] === inputs.fillBtn
-    || inputs[input] === inputs.incShadeBtn
-    || inputs[input] === inputs.decShadeBtn
-    || inputs[input] === inputs.rainbowBtn) {
-    inputs[input].addEventListener('click', (e) => swapModes(e));
-  } 
-};
+	if (
+		inputs[input] === inputs.eraserBtn ||
+		inputs[input] === inputs.fillBtn ||
+		inputs[input] === inputs.incShadeBtn ||
+		inputs[input] === inputs.decShadeBtn ||
+		inputs[input] === inputs.rainbowBtn
+	) {
+		inputs[input].addEventListener("click", (e) => swapModes(e));
+	}
+}
 
 const displays = {
-  colorDisplay: document.querySelector("#colorDisplay"),
-  hexLabel: document.querySelector("#hexLabel"),
-  rgbLabel: document.querySelector("#rgbLabel"),
-  hslLabel: document.querySelector("#hslLabel")
+	colorDisplay: document.querySelector("#colorDisplay"),
+	hexLabel: document.querySelector("#hexLabel"),
+	rgbLabel: document.querySelector("#rgbLabel"),
+	hslLabel: document.querySelector("#hslLabel"),
 };
 
 const containers = {
-  grid: document.querySelector('.sketchContainer'),
-  sizeDisplay: document.getElementById('rangeOutput')
+	grid: document.querySelector(".sketchContainer"),
+	sizeDisplay: document.getElementById("rangeOutput"),
 };
 
 const current = {
-  color: inputs.colorPicker.value,
-  gridSize: inputs.sizeSlider.value
+	color: inputs.colorPicker.value,
+	gridSize: inputs.sizeSlider.value,
 };
 
+// Mouse events
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+// Touch Events
+document.body.ontouchstart = () => (mouseDown = true);
+document.body.ontouchend = () => (mouseDown = false);
+
 const eventListeners = [
-  inputs.clearBtn.addEventListener('click', resetGrid),
-  inputs.sizeSlider.addEventListener('click', (e) => {
-    updateSizeDisplay(e.target.value);
-  }),
-  inputs.sizeSlider.addEventListener('change', (e) => {
-    changeSize(e.target.value);
-  }),
-  inputs.colorPicker.addEventListener('input', (e) => {
-    newColor(e.target.value);
-    updateColorDisplay();
-  }),
-  inputs.hexBox.addEventListener("click", function() {
-    updateColorDisplay();
-  }),
-  inputs.rgbBox.addEventListener("click", function() {
-    updateColorDisplay();
-  }),
-  inputs.hslBox.addEventListener("click", function() {
-    updateColorDisplay();
-  })
+	inputs.clearBtn.addEventListener("click", resetGrid),
+	inputs.sizeSlider.addEventListener("click", (e) => {
+		updateSizeDisplay(e.target.value);
+	}),
+	inputs.sizeSlider.addEventListener("change", (e) => {
+		changeSize(e.target.value);
+	}),
+	inputs.colorPicker.addEventListener("input", (e) => {
+		newColor(e.target.value);
+		updateColorDisplay();
+	}),
+	inputs.hexBox.addEventListener("click", function () {
+		updateColorDisplay();
+	}),
+	inputs.rgbBox.addEventListener("click", function () {
+		updateColorDisplay();
+	}),
+	inputs.hslBox.addEventListener("click", function () {
+		updateColorDisplay();
+	}),
 ];
 
 function makeGrid(size) {
-  containers.grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  containers.grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+	containers.grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	containers.grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-
-  for (let i = 0; i < size * size; i++) {
-    let gridBox = document.createElement('div');
-    gridBox.classList.add('grid');
-    gridBox.addEventListener('mouseover', changeColor);
-    gridBox.addEventListener('mousedown', changeColor);
-    containers.grid.appendChild(gridBox);
-    gridBox.style.opacity = 0.0;
-  }
-};
+	for (let i = 0; i < size * size; i++) {
+		let gridBox = document.createElement("div");
+		gridBox.classList.add("grid");
+		gridBox.addEventListener("mouseover", changeColor);
+		gridBox.addEventListener("mousedown", changeColor);
+		gridBox.addEventListener("touchstart", changeColor);
+		gridBox.addEventListener("touchmove", changeColor);
+		containers.grid.appendChild(gridBox);
+		gridBox.style.opacity = 0.0;
+	}
+}
 
 function resetGrid() {
-  clearGrid();
-  makeGrid(current.gridSize);
-};
+	clearGrid();
+	makeGrid(current.gridSize);
+}
 
 function newColor(newColor) {
-  current.color = newColor;
-};
+	current.color = newColor;
+}
 
 function changeSize(newSize) {
-  current.gridSize = newSize;
-  updateSizeDisplay(newSize);
-  clearGrid();
-  makeGrid(current.gridSize);
-};
+	current.gridSize = newSize;
+	updateSizeDisplay(newSize);
+	clearGrid();
+	makeGrid(current.gridSize);
+}
 
 function updateSizeDisplay(gridSize) {
-  containers.sizeDisplay.innerHTML = `${gridSize} x ${gridSize}`;
-};
+	containers.sizeDisplay.innerHTML = `${gridSize} x ${gridSize}`;
+}
 
 function clearGrid() {
-  containers.grid.innerHTML = '';
-};
+	containers.grid.innerHTML = "";
+}
 
 function changeColor(e) {
-  if (e.type === 'mouseover' && !mouseDown) return;
-  if (inputs.fillBtn.classList.contains("button-active")) {
-    e.target.style.backgroundColor = current.color;
-    e.target.style.opacity = 1;
-  } else if (inputs.rainbowBtn.classList.contains("button-active")) {
-    const randomColor = () => Math.floor(Math.random() * 256)
-    e.target.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
-    e.target.style.opacity = 1;
-  } else if (inputs.incShadeBtn.classList.contains("button-active")) {
-    if (e.target.style.opacity < 1) {
-      e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.1;
-    }
-  } else if (inputs.decShadeBtn.classList.contains("button-active")) {
-    if (e.target.style.opacity > 0) {
-      e.target.style.opacity -= 0.1;
-    }
-  } else if (inputs.eraserBtn.classList.contains("button-active")) {
-    e.target.style.backgroundColor = 'rgba(0, 0, 0, 1)';
-    e.target.style.opacity = 0.0;
-  }
-};
+	if (e.type.startsWith("touch")) e.preventDefault();
+	if (e.type === "mouseover" && !mouseDown) return;
+	const box = e.target;
+	if (inputs.fillBtn.classList.contains("button-active")) {
+		box.style.backgroundColor = current.color;
+		box.style.opacity = 1;
+	} else if (inputs.rainbowBtn.classList.contains("button-active")) {
+		const randomColor = () => Math.floor(Math.random() * 256);
+		box.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+		box.style.opacity = 1;
+	} else if (inputs.incShadeBtn.classList.contains("button-active")) {
+		if (box.style.opacity < 1) {
+			box.style.opacity = parseFloat(box.style.opacity) + 0.1;
+		}
+	} else if (inputs.decShadeBtn.classList.contains("button-active")) {
+		if (box.style.opacity > 0) {
+			box.style.opacity -= 0.1;
+		}
+	} else if (inputs.eraserBtn.classList.contains("button-active")) {
+		box.style.backgroundColor = "rgba(0, 0, 0, 1)";
+		box.style.opacity = 0.0;
+	}
+}
 
 function swapModes(e) {
-  if (!e.target.classList.contains("button-active")) {
-    for (const input in inputs) {
-      inputs[input].classList.remove("button-active");
-    }
-    e.target.classList.toggle("button-active");
-  }
-};
+	if (!e.target.classList.contains("button-active")) {
+		for (const input in inputs) {
+			inputs[input].classList.remove("button-active");
+		}
+		e.target.classList.toggle("button-active");
+	}
+}
 
 function hex_to_RGB(hex) {
-  let m = hex.match(/^#?([\da-f]{2})([\da-f]{2})([\da-f]{2})$/i);
-  return {
-    r: parseInt(m[1], 16),
-    g: parseInt(m[2], 16),
-    b: parseInt(m[3], 16)
-  };
-};
+	let m = hex.match(/^#?([\da-f]{2})([\da-f]{2})([\da-f]{2})$/i);
+	return {
+		r: parseInt(m[1], 16),
+		g: parseInt(m[2], 16),
+		b: parseInt(m[3], 16),
+	};
+}
 
 class Color {
-  constructor(r, g, b, name) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-    this.name = name;
-    this.calcHSL();
-  }
-  innerRGB() {
-    const { r, g, b } = this;
-    return `${r}, ${g}, ${b}`;
-  }
-  rgb() {
-    return `rgb(${this.innerRGB()})`;
-  }
-  rgba(a = 1.0) {
-    return `rgba(${this.innerRGB()}, ${a})`;
-  }
-  hex() {
-    const { r, g, b } = this;
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-  }
-  hsl() {
-    const { h, s, l } = this;
-    return `hsl(${h},${s}%, ${l}%)`;
-  }
-  fullSaturation() {
-    const { h, l } = this;
-    return `hsl(${h},100%, ${l}%)`;
-  }
-  opposite() {
-    const { h, s, l } = this;
-    const newHue = (h + 180) % 360
-    return `hsl(${newHue},${s}%, ${l}%)`;
-  }
-  calcHSL() {
-    let { r, g, b } = this;
-    // Make r, g, and b fractions of 1
-    r /= 255;
-    g /= 255;
-    b /= 255;
+	constructor(r, g, b, name) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.name = name;
+		this.calcHSL();
+	}
+	innerRGB() {
+		const { r, g, b } = this;
+		return `${r}, ${g}, ${b}`;
+	}
+	rgb() {
+		return `rgb(${this.innerRGB()})`;
+	}
+	rgba(a = 1.0) {
+		return `rgba(${this.innerRGB()}, ${a})`;
+	}
+	hex() {
+		const { r, g, b } = this;
+		return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+	}
+	hsl() {
+		const { h, s, l } = this;
+		return `hsl(${h},${s}%, ${l}%)`;
+	}
+	fullSaturation() {
+		const { h, l } = this;
+		return `hsl(${h},100%, ${l}%)`;
+	}
+	opposite() {
+		const { h, s, l } = this;
+		const newHue = (h + 180) % 360;
+		return `hsl(${newHue},${s}%, ${l}%)`;
+	}
+	calcHSL() {
+		let { r, g, b } = this;
+		// Make r, g, and b fractions of 1
+		r /= 255;
+		g /= 255;
+		b /= 255;
 
-    // Find greatest and smallest channel values
-    let cmin = Math.min(r, g, b),
-      cmax = Math.max(r, g, b),
-      delta = cmax - cmin,
-      h = 0,
-      s = 0,
-      l = 0;
-    if (delta == 0) h = 0;
-    else if (cmax == r)
-      // Red is max
-      h = ((g - b) / delta) % 6;
-    else if (cmax == g)
-      // Green is max
-      h = (b - r) / delta + 2;
-    else
-      // Blue is max
-      h = (r - g) / delta + 4;
+		// Find greatest and smallest channel values
+		let cmin = Math.min(r, g, b),
+			cmax = Math.max(r, g, b),
+			delta = cmax - cmin,
+			h = 0,
+			s = 0,
+			l = 0;
+		if (delta == 0) h = 0;
+		else if (cmax == r)
+			// Red is max
+			h = ((g - b) / delta) % 6;
+		else if (cmax == g)
+			// Green is max
+			h = (b - r) / delta + 2;
+		// Blue is max
+		else h = (r - g) / delta + 4;
 
-    h = Math.round(h * 60);
+		h = Math.round(h * 60);
 
-    // Make negative hues positive behind 360°
-    if (h < 0) h += 360;
-    // Calculate lightness
-    l = (cmax + cmin) / 2;
+		// Make negative hues positive behind 360°
+		if (h < 0) h += 360;
+		// Calculate lightness
+		l = (cmax + cmin) / 2;
 
-    // Calculate saturation
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+		// Calculate saturation
+		s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
 
-    // Multiply l and s by 100
-    s = +(s * 100).toFixed(1);
-    l = +(l * 100).toFixed(1);
-    this.h = h;
-    this.s = s;
-    this.l = l;
-  }
+		// Multiply l and s by 100
+		s = +(s * 100).toFixed(1);
+		l = +(l * 100).toFixed(1);
+		this.h = h;
+		this.s = s;
+		this.l = l;
+	}
 }
 
 function convertColor() {
-  const convertedColor = hex_to_RGB(current.color);
-  return secondColor = new Color(convertedColor.r, convertedColor.g, convertedColor.b);
+	const convertedColor = hex_to_RGB(current.color);
+	return (secondColor = new Color(convertedColor.r, convertedColor.g, convertedColor.b));
 }
 
 function updateColorDisplay() {
-  convertColor();
-  if(inputs.hexBox.checked) {
-  displays.colorDisplay.innerHTML = inputs.colorPicker.value;
-  } else if(inputs.rgbBox.checked) {
-    displays.colorDisplay.innerHTML = `${secondColor.rgb()}`;
-  } else if(inputs.hslBox.checked) {
-    displays.colorDisplay.innerHTML = `${secondColor.hsl()}`;
-  }
-  displays.colorDisplay.style.color = inputs.colorPicker.value;
-  displays.hexLabel.style.color = inputs.colorPicker.value;
-  displays.rgbLabel.style.color = inputs.colorPicker.value;
-  displays.hslLabel.style.color = inputs.colorPicker.value;
+	convertColor();
+	if (inputs.hexBox.checked) {
+		displays.colorDisplay.innerHTML = inputs.colorPicker.value;
+	} else if (inputs.rgbBox.checked) {
+		displays.colorDisplay.innerHTML = `${secondColor.rgb()}`;
+	} else if (inputs.hslBox.checked) {
+		displays.colorDisplay.innerHTML = `${secondColor.hsl()}`;
+	}
+	displays.colorDisplay.style.color = inputs.colorPicker.value;
+	displays.hexLabel.style.color = inputs.colorPicker.value;
+	displays.rgbLabel.style.color = inputs.colorPicker.value;
+	displays.hslLabel.style.color = inputs.colorPicker.value;
 }
 
 updateColorDisplay();
